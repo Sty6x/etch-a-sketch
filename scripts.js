ccd6f7c9d58e80ce2fbd8433375a2910b5f92a1b
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-04-08 01:00:16 +0400

    added div boxes

--- scripts.js
+++ scripts.js
@@ -1,25 +1,18 @@
 // const gridContainer = document.getElementsByClassName('grid-container');
 const gridContainer = document.querySelector('.grid-container');
-const gridRows = document.createElement('div');
-const gridCol = document.createElement('div');
-gridRows.classList.toggle('grids');
-gridCol.classList.toggle('grids');
-// gridRows.style.border = '2px solid red';
 
 
-let col = 4;
-let row = 4;
 
 
-for (let i = 0; i < col; i++) {
-    console.log(i)
-    gridContainer.appendChild(gridCol);
-    gridCol.style.border = '2px solid red';
-    gridCol.textContent = `${i}`
-    for (let j = 0; j < row; j++) {
-        gridContainer.appendChild(gridRows);
-        gridRows.style.border = '2px solid red';
-        gridRows.textContent = `${j}`
-        console.log(j)
+
+const createGrid = (gridSize) => {
+    for (let i = 0; i < gridSize; i++) {
+        console.log(i)
+        let grids = document.createElement('div');
+        grids.classList.toggle('grids');
+        grids.style.border = '2px solid red';
+        gridContainer.appendChild(grids);
     }
 }
+
+createGrid(16);
