3412fd32896d86e7425185e1fcb765247531d80d
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-04-12 10:01:06 +0400

    Gave up

--- scripts.js
+++ scripts.js
@@ -1,50 +1,53 @@
 // const gridContainer = document.getElementsByClassName('grid-container');
 const gridContainer = document.querySelector('.grid-container');
 const resetButton = document.createElement('button');
+const setGridSizeButton = document.createElement('button');
+const buttons = document.querySelector('.buttons');
 const body = document.querySelector('body');
+
 const header = document.querySelector('header');
+
 resetButton.textContent = "Reset Grid";
+setGridSizeButton.textContent = 'Set Grid Size';
 
-body.insertBefore(resetButton, gridContainer);
+
+//prompts grid size input on reset
+buttons.appendChild(setGridSizeButton,gridContainer);
 
 
-// todo
-//make container have a fixed size and grids have flex grow
+// console.log(size);
 
-const createGrid = (gridSize, Size) => {
+const createGrid = (gridSize) => {
     let grids;
     for (let i = 0; i < gridSize; i++) {
-        // grids = document.createElement('div');
-        // grids.setAttribute('class', 'grids grid-column')
-        // grids.style.padding = Size + 'px';
-        // gridContainer.appendChild(grids);
-        // drawGrid(grids);
-        // resetGrid(grids);
         for (let j = 0; j < gridSize; j++) {
             grids = document.createElement('div');
             grids.setAttribute('class', 'grids grid-column')
-            grids.style.padding = Size + 'px';
+            // grids.style.padding =  Size + 'px';
             gridContainer.appendChild(grids);
-            drawGrid(grids);
+            drawGrid(grids,i,j);
             resetGrid(grids);
         }
-        resetGrid(grids);
     }
+    
 }
 
 
-function drawGrid(grids) {
-    grids.addEventListener('mouseenter', () => grids.classList.add('colored-grid'))
-}
-
-function resetGrid(grid) {
-    resetButton.addEventListener('click', () => {
-        grid.classList.remove('colored-grid');
-        console.log('clicked remove')
+function drawGrid(grids,pos1, pos2) {
+    grids.addEventListener('mouseenter', () => {
+        console.log(pos1, pos2);
+        grids.classList.add('colored-grid')
     })
 }
 
+buttons.appendChild(resetButton);
+function resetGrid(grid) {
+    resetButton.addEventListener('click', (e) => {
+        grid.classList.remove('colored-grid');
+        // console.log(e);
+    })
+}
 
-//bug overflowing second parameter(paddingSize) is above 15
+//bug overflowing second parameter(paddingSize) 
 //bug asymmetrical grids first parameter(gridSize) is change differently
-createGrid(12, 26);
+changeSize(16);
